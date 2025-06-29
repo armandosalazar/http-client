43f99f6ea9f24ee58fe391734c0b5986ece6faac
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2025-06-29 00:08:42 -0600

    feat: add Select component to HomeView and update layout; include options for selection

--- src/view/home/home-view.tsx
+++ src/view/home/home-view.tsx
@@ -1,9 +1,20 @@
 import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 export default function HomeView() {
 
-  return <>
-    <h1 className="">Hola mundo</h1>
+  return <section className="container mx-auto p-4">
+    <article className="flex">
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Options" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="option1">Option 1</SelectItem>
+          <SelectItem value="option2">Option 2</SelectItem>
+        </SelectContent>
+      </Select>
     <Input />
-  </>
+    </article>
+  </section>;
 }
