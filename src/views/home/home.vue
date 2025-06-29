251e28110763948e5abe7b70fb3d2b928e4ffdf7
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2025-06-28 22:53:31 -0600

    fix: adjust layout of form elements in home view and comment out header

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -18,13 +18,13 @@ async function sendRequest() {
 
 <template>
   <div class="p-3">
-    <h1 class="font-bold">
+    <!-- <h1 class="font-bold">
       Http Client
-    </h1>
+    </h1> -->
     <div>
       <form @submit.prevent="sendRequest" class="flex gap-2">
-        <n-select class="flex-1" :options="methods" />
-        <n-input placeholder="Enter URL" class="flex-1/2" />
+        <n-select class="w-24" :options="methods" />
+        <n-input placeholder="Enter URL" class="w-full" />
       </form>
     </div>
   </div>
